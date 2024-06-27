70f625010b8bcc3cc26f384cd0f3b9cc39ee8a4d
gpmatheus <gp.matheusluis@gmail.com>
2024-06-27 18:23:49 -0300

    added args to program

--- linear-regression/mf_linear_regression.py
+++ linear-regression/mf_linear_regression.py
@@ -1,8 +1,23 @@
 import pandas
 import numpy as np
+import argparse
+import sys
 
-data = pandas.read_csv('multiple_feature_data.csv')
-data.insert(0, 'x0', np.ones(data.shape[0]))
+parser = argparse.ArgumentParser(
+    prog="Multiple Feature Linear Regression",
+    description="Trains a Linear Model to Match Data")
+parser.add_argument('--filename', action='store', nargs='?', required=True, type=str)
+parser.add_argument('--x0name', action='store', nargs='?', required=False, default='x0', type=str)
+parser.add_argument('--target', action='store', nargs='?', required=True, type=str)
+parser.add_argument('--seed', action='store', nargs='?', required=False, default=1, type=int)
+parser.add_argument('--alpha', action='store', nargs='?', required=False, default=0.01, type=float)
+parser.add_argument('--max-dvt', action='store', nargs='?', required=False, default=0.01, type=float)
+
+sys.argv.pop(0)
+args = parser.parse_args(sys.argv)
+
+data = pandas.read_csv(args.filename)
+data.insert(0, args.x0name, np.ones(data.shape[0]))
 
 def create_hypothesis(theta):
     def hypothesis(X):
@@ -12,18 +27,18 @@ def create_hypothesis(theta):
 def dvt(data, h):
     def derivative(X, y, j):
         return (h(X) - y) * X[j]
-    X = data.drop('y', axis=1)
-    Y = data['y']
+    X = data.drop(args.target, axis=1)
+    Y = data[args.target]
     data_len = X.shape[0]
     return np.array([np.sum([derivative(x, y, j) for x, y in zip(X.values, Y)]) / data_len for j in range(X.shape[1])])
 
-# np.random.seed(0)
+np.random.seed(args.seed)
 theta = np.random.rand(data.shape[1] - 1)
 h = create_hypothesis(theta)
 derivatives = dvt(data, h)
 
-alpha = 0.001
-max_dvt = 0.0001
+alpha = args.alpha
+max_dvt = args.max_dvt
 while np.all(np.abs(derivatives) > max_dvt):
     derivatives *= alpha
     theta -= derivatives
@@ -34,22 +49,25 @@ while np.all(np.abs(derivatives) > max_dvt):
 print(theta)
 
 # plot result
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+data = data.drop(args.x0name, axis=1)
+x_data = data.drop(args.target, axis=1)
+if len(x_data.axes[1]) == 2:
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
 
-data = data.drop('x0', axis=1)
-x1 = data['x1']
-x2 = data['x2']
-y = data['y']
-ax.scatter(x1, x2, y, color='b', marker='o', label='Data')
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-x_surface = np.linspace(min(x1), max(x1), 2)
-y_surface = np.linspace(min(x2), max(x2), 2)
-x_surface, y_surface = np.meshgrid(x_surface, y_surface)
-z_surface = theta[0] + x_surface * theta[1] + y_surface * theta[2]
-ax.plot_surface(x_surface, y_surface, z_surface, color='r', alpha=0.5)
+    x1 = x_data.iloc[:, 0]
+    x2 = x_data.iloc[:, 1]
+    y = data[args.target]
+    ax.scatter(x1, x2, y, color='b', marker='o', label='Data')
 
-plt.show()
+    x_surface = np.linspace(min(x1), max(x1), 2)
+    y_surface = np.linspace(min(x2), max(x2), 2)
+    x_surface, y_surface = np.meshgrid(x_surface, y_surface)
+    z_surface = theta[0] + x_surface * theta[1] + y_surface * theta[2]
+    ax.plot_surface(x_surface, y_surface, z_surface, color='r', alpha=0.5)
+
+    plt.show()
